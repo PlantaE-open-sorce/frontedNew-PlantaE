289ecbf418fb8e1791bdd2d56300942dc711489f
AntonioNavarro24 <137426429+AntonioNavarro24@users.noreply.github.com>
2025-10-27 01:55:13 -0500

    feat(auth): add auth model.

---
+++ src/app/plant-care/domain/models/auth.model.ts
@@ -0,0 +1,12 @@
+export interface AuthCredentials {
+  username: string;
+  password: string;
+}
+
+export interface RegistrationPayload {
+  firstName: string;
+  lastName: string;
+  email: string;
+  username: string;
+  password: string;
+}
